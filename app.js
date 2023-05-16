e1191bb61fb44df12fd3c4b176e16cdfc42ab526
NadiaTurko <47818620+NadiaTurko@users.noreply.github.com>
2023-05-16 12:46:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
-const kupeNumber = function(seatNumber){
-    if(seatNumber > 0 && seatNumber < 37){
+let seatNumber = prompt("Введіть номер місця, який вказаний в білеті");
+
+const getKupeNumber = function(seatNumber){
+    if(seatNumber > 0 && seatNumber < 36){
     return Math.round(++seatNumber/4);
 }
     else if(seatNumber > 36 && seatNumber < 54){
     return Math.round((54 - seatNumber)/2);
-    }
-}
+    } 
+};
 
 
-let seatNumber = prompt("Введіть номер місця, який вказаний в білеті");
-if (seatNumber % 2 != 0 && seatNumber > 0 && seatNumber < 36){
-    alert (` Ваше місце номер ${seatNumber} є нижнім в ${kupeNumber(seatNumber)} купе`);
-}
-else if (seatNumber % 2 == 0 && seatNumber > 0 && seatNumber < 36 ){
-    alert (` Ваше місце номер ${seatNumber} є верхнім в ${kupeNumber(seatNumber)} купе`);
-}
-else if (seatNumber % 2 != 0 && seatNumber > 36 && seatNumber < 54 ){
-    alert (` Ваше місце номер ${seatNumber} є нижнім боковим в ${kupeNumber(seatNumber)} купе`);
-}
-else if (seatNumber % 2 == 0 && seatNumber > 36 && seatNumber < 54 ){
-    alert (` Ваше місце номер ${seatNumber} є верхнім боковим в ${kupeNumber(seatNumber)} купе`);
-}
-else{
-    alert ("Номер місця вказаний не вірно");
-}
+const getSeatNumber = seatNumber => (seatNumber % 2 == 0 && seatNumber > 0 && seatNumber < 36) ? "верхнім" : "нижнім";
+const getSideSeatNumber = seatNumber => (seatNumber % 2 == 0 && seatNumber > 35 && seatNumber < 54) ? "бічним верхнім" : "бічним нижнім";
 
+
+if (seatNumber > 0 && seatNumber < 36){
+    alert (` Ваше місце номер ${seatNumber} є ${getSeatNumber(seatNumber)} в ${getKupeNumber(seatNumber)} купе`);
+} else if (seatNumber > 36 && seatNumber < 54){
+    alert (` Ваше місце номер ${seatNumber} є ${getSideSeatNumber(seatNumber)} в ${getKupeNumber(seatNumber)} купе`);
+} else{
+   alert("Ви вказали місце невірно");
+};
