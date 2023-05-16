ee3fbca368b44addc2a7022fa77e495b35c74038
NadiaTurko <47818620+NadiaTurko@users.noreply.github.com>
2023-05-16 20:48:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 let seatNumber = prompt("Введіть номер місця, який вказаний в білеті");
-
 const getKupeNumber = function(seatNumber){
-    if(seatNumber > 0 && seatNumber < 36){
+    if(seatNumber > 0 && seatNumber < 37){
     return Math.round(++seatNumber/4);
 }
-    else if(seatNumber > 36 && seatNumber < 54){
-    return Math.round((54 - seatNumber)/2);
+    else if(seatNumber > 36 && seatNumber < 55){
+    return Math.floor(((55 - seatNumber)/2));
     } 
-};
-
-
-const getSeatNumber = seatNumber => (seatNumber % 2 == 0 && seatNumber > 0 && seatNumber < 36) ? "верхнім" : "нижнім";
-const getSideSeatNumber = seatNumber => (seatNumber % 2 == 0 && seatNumber > 35 && seatNumber < 54) ? "бічним верхнім" : "бічним нижнім";
-
-
+}
+const getSeatNumber = seatNumber => (seatNumber % 2 == 0 ) ? "верхнім" : "нижнім";
+const getSideSeatNumber = seatNumber => (seatNumber % 2 == 0) ? "бічним верхнім" : "бічним нижнім";
 if (seatNumber > 0 && seatNumber < 36){
     alert (` Ваше місце номер ${seatNumber} є ${getSeatNumber(seatNumber)} в ${getKupeNumber(seatNumber)} купе`);
 } else if (seatNumber > 36 && seatNumber < 54){
